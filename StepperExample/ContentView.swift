4b0ede9fbeadcf770f46f4eec9fc847ea57ab872
Courage Zeng <czeng@lcs.on.ca>
2025-01-13 09:31:16 -0500

    Fixed the user interface in this app so that it presents mathematically correct statements at all times.

--- StepperExample/ContentView.swift
+++ StepperExample/ContentView.swift
@@ -4,13 +4,13 @@
 //
 //  Created by 曾梓恒 on 2025-01-13.
 //
- 
+
 import SwiftUI
- 
+
 struct ContentView: View {
     
     // MARK: Stored properties
- 
+    
     @State var base: Int = 1
     
     // MARK: Computed properties
@@ -19,38 +19,48 @@ struct ContentView: View {
         return base * base
     }
     
+    var formattedBase: String {
+        if base < 0 {
+            return "(\(base))"
+        } else {
+            return "\(base)"
+        }
+    }
+    
     var body: some View {
         VStack {
             
             Spacer()
             
             HStack(alignment: .top) {
-                 
-                Text("\(base)")
+                
+                Text("\(formattedBase)")
                     .font(.system(size: 96))
- 
+                
                 Text("2")
                     .font(.system(size: 44))
- 
+                
                 Text("=")
                     .font(.system(size: 96))
- 
+                
                 Text("\(squared)")
                     .font(.system(size: 96))
             }
+            .lineLimit(1)
+            .minimumScaleFactor(0.5)
             
             Stepper(value: $base, label: {
                 Text("Base")
             })
-
-                        
+            
+            
             Spacer()
         }
         .padding()
     }
 }
- 
+
 #Preview {
     ContentView()
 }
- 
+
